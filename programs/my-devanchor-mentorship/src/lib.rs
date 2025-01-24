da3350a36cf03b6c27ae2c5144259ab0e4428795
Olajumoke Emmanuel <41354996+Emmyhack@users.noreply.github.com>
2025-01-24 15:51:31 -0800

    Update lib.rs

--- programs/my-devanchor-mentorship/src/lib.rs
+++ programs/my-devanchor-mentorship/src/lib.rs
@@ -1,16 +1,31 @@
 use anchor_lang::prelude::*;
 
-declare_id!("4DKsyTYThSeZgoYP7We2Y8gmkfQAxobShwJmz1brgsGz");
+declare_id!("93XNRZ55njk1GPtTWY9Jdm3ko1iqPcWN5QophZr9krTU");
 
 #[program]
-pub mod my_devanchor_mentorship {
+pub mod anchor_escrow {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    // buy function
+    pub fn buy(ctx: Context<Buy>, product_name: String, amount: u64) -> Result<()> {
+        Ok(()) // Signals successful transaction
+    }
+    // sell function
+    pub fn sell(ctx: Context<Sell>, product_name: String, amount: u64) -> Result<()> {
+        Ok(()) // Signals successful transaction
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Buy {}
+
+#[derive(Accounts)]
+pub struct Sell {}
